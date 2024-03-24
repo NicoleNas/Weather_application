3b7fc66a1704b7d965072f742478c7621f0427d5
Nikoletta Naoumi <nNaoumi@mrhealthtech.com>
2024-03-24 18:33:32 +0200

    complete static view layout

--- src/js/viewModels/home.js
+++ src/js/viewModels/home.js
@@ -1,15 +1,42 @@
-define(['../accUtils'],
- function(accUtils) {
+define(['knockout', 'ojs/ojchart', 'ojs/ojcontext', 'ojs/ojmodule-element-utils', 'ojs/ojresponsiveutils', 'ojs/ojresponsiveknockoututils', 'ojs/ojcorerouter', 'ojs/ojarraydataprovider', 'ojs/ojknockouttemplateutils', 'ojs/ojmodule-element', 'ojs/ojknockout'],
+ function(ko, ojChart, Context, moduleUtils, ResponsiveUtils, ResponsiveKnockoutUtils, CoreRouter, ArrayDataProvider, KnockoutTemplateUtils) {
     function DashboardViewModel() {
       const self = this;
+
+      self.isNowSelected = ko.observable(true);
+      self.isTodaySelected = ko.observable(false);
+      self.isDateSelected = ko.observable(false);
+
+      self.isTodaySelectedClicked = function () {
+        self.isNowSelected(false);
+        self.isTodaySelected(true);
+        self.isDateSelected(false);
+      };
+
+      self.isDateSelectedClicked = function () {
+        self.isNowSelected(false);
+        self.isTodaySelected(false);
+        self.isDateSelected(true);
+      };
+
+      self.isNowSelectedClicked = function () {
+        self.isNowSelected(true);
+        self.isTodaySelected(false);
+        self.isDateSelected(false);
+      };
+
+      self.temperatureData = ko.observableArray([
+        {items: [
+          (1,16), (2,18), (3,21), (4,17)
+        ]}
+      ]);
+
+      self.chartGroups = ko.observableArray(["Day 1", "Day 2", "Day 3", "Day 4"]);
 
       // add code here
 
       self.connected = () => {
-        accUtils.announce('Dashboard page loaded.');
-        document.title = "Home";
-        // Implement further logic if needed
-      };
+       };
 
       /**
        * Optional ViewModel method invoked after the View is disconnected from the DOM.
