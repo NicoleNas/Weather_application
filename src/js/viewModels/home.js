717a9d5e746346491c1a9620dd2b65e4e609a9dc
Nikoletta Naoumi <nNaoumi@mrhealthtech.com>
2024-03-25 17:34:29 +0200

    extra layout changes API call, enable Not working, 3rd Layout needs fixes

--- src/js/viewModels/home.js
+++ src/js/viewModels/home.js
@@ -1,64 +1,177 @@
-define(['knockout', 'ojs/ojchart', 'ojs/ojcontext', 'ojs/ojmodule-element-utils', 'ojs/ojresponsiveutils', 'ojs/ojresponsiveknockoututils', 'ojs/ojcorerouter', 'ojs/ojarraydataprovider', 'ojs/ojknockouttemplateutils', 'ojs/ojmodule-element', 'ojs/ojknockout'],
- function(ko, ojChart, Context, moduleUtils, ResponsiveUtils, ResponsiveKnockoutUtils, CoreRouter, ArrayDataProvider, KnockoutTemplateUtils) {
-    function DashboardViewModel() {
-      const self = this;
+define([
+  "knockout",
+  "ojs/ojchart",
+  "ojs/ojdatetimepicker",
+  "ojs/ojcontext",
+  "ojs/ojmodule-element-utils",
+  "ojs/ojresponsiveutils",
+  "ojs/ojresponsiveknockoututils",
+  "ojs/ojcorerouter",
+  "ojs/ojarraydataprovider",
+  "ojs/ojknockouttemplateutils",
+  "ojs/ojmodule-element",
+  "ojs/ojknockout",
+], function (
+  ko,
+  ojChart,
+  InputDateElement,
+  ResponsiveUtils,
+  ResponsiveKnockoutUtils,
+  ArrayDataProvider,
+  KnockoutTemplateUtils
+) {
+  function DashboardViewModel() {
+    const self = this;
 
-      self.isNowSelected = ko.observable(true);
-      self.isTodaySelected = ko.observable(false);
-      self.isDateSelected = ko.observable(false);
+    self.isNowSelected = ko.observable(true);
+    self.isTodaySelected = ko.observable(false);
+    self.isDateSelected = ko.observable(false);
+    self.isApplyClicked = ko.observable(false);
+    self.selectedDate = ko.observable(null);
+    self.selectedVal = ko.observable();
 
-      self.isTodaySelectedClicked = function () {
-        self.isNowSelected(false);
-        self.isTodaySelected(true);
-        self.isDateSelected(false);
-      };
+    self.currentWeather = {
+      temp: ko.observable(null),
+      feels_like: ko.observable(null),
+      pressure: ko.observable(null),
+      humidity: ko.observable(null),
+      wind_speed: ko.observable(null),
+      wind_deg: ko.observable(null),
+      wind_gust: ko.observable(null),
+      description: ko.observable(null),
+      icon: ko.observable(null),
+    };
 
-      self.isDateSelectedClicked = function () {
-        self.isNowSelected(false);
-        self.isTodaySelected(false);
-        self.isDateSelected(true);
-      };
+    self.forecastWeather = {
+      tempAvg: ko.observable(null),
+      feelsLikeAvg: ko.observable(null),
+      pressure: ko.observable(null),
+      humidity: ko.observable(null),
+      wind_speed: ko.observable(null),
+      wind_deg: ko.observable(null),
+      wind_gust: ko.observable(null),
+      description: ko.observable(null),
+      icon: ko.observable(null)
+  };
 
-      self.isNowSelectedClicked = function () {
-        self.isNowSelected(true);
-        self.isTodaySelected(false);
-        self.isDateSelected(false);
-      };
+  self.temperatureData = ko.observableArray([]);
+  self.chartGroups = ko.observableArray([]);
 
-      self.temperatureData = ko.observableArray([
-        {items: [
-          (1,16), (2,18), (3,21), (4,17)
-        ]}
-      ]);
+    self.selectedVal.subscribe(function (value) {
+      self.selectedDate(value);
+      self.isApplyButtonEnabled();
+    });
 
-      self.chartGroups = ko.observableArray(["Day 1", "Day 2", "Day 3", "Day 4"]);
+    self.isApplyButtonEnabled = ko.computed(function () {
+      return self.selectedDate() !== null && self.selectedDate() !== "";
+    });
 
-      // add code here
+    self.isTodaySelectedClicked = function () {
+      self.isNowSelected(false);
+      self.isTodaySelected(true);
+      self.isDateSelected(false);
+      self.isApplyClicked(false);
+    };
 
-      self.connected = () => {
-       };
+    self.isDateSelectedClicked = function () {
+      self.isNowSelected(false);
+      self.isTodaySelected(false);
+      self.isDateSelected(!self.isDateSelected());
+    };
 
-      /**
-       * Optional ViewModel method invoked after the View is disconnected from the DOM.
-       */
-      self.disconnected = () => {
-        // Implement if needed
-      };
+    self.isNowSelectedClicked = function () {
+      self.isNowSelected(true);
+      self.isTodaySelected(false);
+      self.isDateSelected(false);
+      self.isApplyClicked(false);
+    };
 
-      /**
-       * Optional ViewModel method invoked after transition to the new View is complete.
-       * That includes any possible animation between the old and the new View.
-       */
-      self.transitionCompleted = () => {
-        // Implement if needed
-      };
+    self.applyDate = function () {
+      self.isApplyClicked(!self.isApplyClicked());
+    };
+
+    function getCurrentAndFiveDaysAhead() {
+      const currentDate = new Date();
+      const fiveDaysAhead = new Date();
+      fiveDaysAhead.setDate(currentDate.getDate() + 5);
+
+      const currentDateFormatted = currentDate.toISOString().split("T")[0];
+      const fiveDaysAheadFormatted = fiveDaysAhead.toISOString().split("T")[0];
+
+      return { currentDateFormatted, fiveDaysAheadFormatted };
     }
 
-    /*
-     * Returns an instance of the ViewModel providing one instance of the ViewModel. If needed,
-     * return a constructor for the ViewModel so that the ViewModel is constructed
-     * each time the view is displayed.
-     */
-    return DashboardViewModel;
+    const { currentDateFormatted, fiveDaysAheadFormatted } =
+      getCurrentAndFiveDaysAhead();
+
+    self.minDate = ko.observable(currentDateFormatted);
+    self.maxDate = ko.observable(fiveDaysAheadFormatted);
+
+    self.updateViewModelData = function (data) {
+      self.currentWeather.temp(data.current.temp);
+      self.currentWeather.feels_like(data.current.feels_like);
+      self.currentWeather.pressure(data.current.pressure);
+      self.currentWeather.humidity(data.current.humidity);
+      self.currentWeather.wind_speed(data.current.wind_speed);
+      self.currentWeather.wind_deg(data.current.wind_deg);
+      self.currentWeather.wind_gust(data.current.wind_gust);
+      self.currentWeather.description(data.current.weather[0].description);
+      self.currentWeather.icon(
+        `http://openweathermap.org/img/wn/${data.current.weather[0].icon}.png`
+      );
+    };
+
+    self.updateForecastData = function (data) {
+      const today = data.daily[0];
+      const tempAvg = (today.temp.day + today.temp.eve + today.temp.morn + today.temp.night) / 4;
+      const feelsLikeAvg = (today.feels_like.day + today.feels_like.eve + today.feels_like.morn + today.feels_like.night) / 4;
+
+      self.forecastWeather.tempAvg(tempAvg.toFixed(2));
+      self.forecastWeather.feelsLikeAvg(feelsLikeAvg.toFixed(2));
+      self.forecastWeather.pressure(today.pressure);
+      self.forecastWeather.humidity(today.humidity);
+      self.forecastWeather.wind_speed(today.wind_speed);
+      self.forecastWeather.wind_deg(today.wind_deg);
+      self.forecastWeather.wind_gust(today.wind_gust);
+      self.forecastWeather.description(today.weather[0].description);
+      self.forecastWeather.icon(`http://openweathermap.org/img/wn/${today.weather[0].icon}.png`);
+  };
+
+  self.updateForecastMaxTemperatures = function (data) {
+    const temperatures = data.daily.slice(0, 7).map((day, index) => (index + 1,
+      day.temp.max)
+    );
+
+    const days = data.daily.slice(0, 7).map((day) => {
+      const date = new Date(day.dt * 1000);
+      return `${date.toLocaleDateString()}`;
+    });
+
+    self.temperatureData([ {items: temperatures} ]);
+    console.log(self.temperatureData());
+    self.chartGroups(days);
+  };
+
+    // API call
+    const apiUrl =
+      "https://api.openweathermap.org/data/2.5/onecall?lat=40.58725980318928&lon=22.948223362612612&exclude=hourly,minutely&appid=11b0499bd13ab56063de7565a440eb97&units=metric";
+
+    fetch(apiUrl)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        self.updateViewModelData(data);
+        self.updateForecastData(data);
+        self.updateForecastMaxTemperatures(data);
+      })
+      .catch((error) =>
+        console.error("There has been a problem fetching data", error)
+      );
   }
-);
+  return DashboardViewModel;
+});
